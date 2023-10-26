27389d2949a5529b2343fd295b592d688ba879c9
nuhptr <nugrahaadi733@gmail.com>
2023-10-26 08:47:35 +0700

    layout: update code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { Theme, ThemePanel } from "@radix-ui/themes"
+import { Theme } from "@radix-ui/themes"
 
 import "./globals.css"
 // add this if you use radix-ui
@@ -12,14 +12,14 @@ import Navbar from "./components/Navbar"
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
 
 export const metadata: Metadata = {
-   title: "Issue Tracker | Home",
+   title: "Issue Tracker",
    description: "Build a simple issue tracker with Next.js and Prisma",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <html lang="en" suppressHydrationWarning>
-         <body className={inter.variable}>
+         <body className={inter.variable} suppressHydrationWarning>
             <Theme appearance="light" accentColor="blue" radius="large">
                <Navbar />
                <main className="p-5">{children}</main>
